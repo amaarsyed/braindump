0b6148d1f3b99c9717c10395a888fccfb693b26f
Amaar <58195073+amaarshah@users.noreply.github.com>
2025-05-15 00:19:52 -0400

    Dark Mode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
+  darkMode: "class", // <- this enables class-based dark mode
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
