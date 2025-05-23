e14f268de57e121295c154db8bf428b1cb90cc97
Amaar <58195073+amaarshah@users.noreply.github.com>
2025-05-23 18:01:32 -0400

    Restore project to previous working whiteboard state and remove TypeScript files (never migrating again lol)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CanvasPage from "./pages/CanvasPage";
+import ChatBot from "./components/ChatBot";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasPage />
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App; 
