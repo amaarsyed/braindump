aa457dd4599608d5843d08b23e978247f26fda8d
Amaar <58195073+amaarshah@users.noreply.github.com>
2025-05-24 17:56:17 -0400

    fixed the ui for the cusror ux for it aswell for smooth movement and interactions in between

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './App.css';
 import CanvasPage from "./pages/CanvasPage"
 // import ChatBot from "./components/ChatBot"
-// import { CursorifyProvider } from '@cursorify/react';
+import { CursorifyProvider } from '@cursorify/react';
 import CustomCursor from './components/CustomCursor';
 
 function App() {
   return (
-    // <CursorifyProvider cursor={<CustomCursor />}>
+    <CursorifyProvider
+      cursor={<CustomCursor />}
+      delay={0}
+      smoothness={0.2}
+      hideNativeCursor={true}
+    >
       <div className="h-screen w-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-dark text-black dark:text-white transition-colors duration-300">
-        <CustomCursor />
         <CanvasPage />
         {/* <ChatBot /> */}
       </div>
-    // </CursorifyProvider>
-  )
+    </CursorifyProvider>
+  );
 }
 
-export default App
+export default App;
